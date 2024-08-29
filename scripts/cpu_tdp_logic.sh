34c91054f01ce0777ea727fe3ca3ca83c57c63bc
Nikolay <deluxoft@gmail.com>
2024-08-30 00:00:01 +0300

    Backuped by cron

--- scripts/cpu_tdp_logic.sh
+++ scripts/cpu_tdp_logic.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 
-action="$1"
-case $action in
-  "ac_plug_in")
-    sudo /home/nikolay/.local/bin/ryzenadj --slow-limit=25000 --stapm-limit=25000 --fast-limit=40000 --tctl-temp=85
-    ;;
+# action="$1"
+# case $action in
+#   "ac_plug_in")
+#     sudo /home/nikolay/.local/bin/ryzenadj --slow-limit=25000 --stapm-limit=25000 --fast-limit=40000 --tctl-temp=85
+#     ;;
 
-  "ac_plug_out")
-    sudo /home/nikolay/.local/bin/ryzenadj --slow-limit=8000 --stapm-limit=10000 --fast-limit=12000 --tctl-temp=60
-    ;;
-esac
+#   "ac_plug_out")
+#     sudo /home/nikolay/.local/bin/ryzenadj --slow-limit=8000 --stapm-limit=10000 --fast-limit=12000 --tctl-temp=60
+#     ;;
+# esac
 
 date
 sudo tlp-stat -p
