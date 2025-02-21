06d10f5c5cd2927bbb25ce28132c30e125266162
Nikolay <deluxoft@gmail.com>
2025-02-22 00:00:01 +0300

    Backuped by cron

--- scripts/gen_multiplying_table.py
+++ scripts/gen_multiplying_table.py
@@ -2,14 +2,16 @@
 
 import csv
 from pathlib import Path
-from random import randint
+from random import randint, choice
 
 
 def main():
-    SAMPLES_PER_BEGUNOK, N_BEGUNOKS = 38, 21
+    SAMPLES_PER_BEGUNOK, N_BEGUNOKS = 38, 25
     ANSWER_WIDTH = 8
     OUTPUT_FILES_DIR = Path(__file__).parent / "begunoks"
     OUTPUT_FILES_DIR.mkdir(exist_ok=True)
+
+    exps = [10**i for i in range(3)]
 
     with (OUTPUT_FILES_DIR / "begunoks.csv").resolve().open(
         "w", newline=str()
@@ -19,10 +21,11 @@ def main():
             row = []
             for j in range(N_BEGUNOKS):
                 a, b = randint(2, 10), randint(2, 10)
+                ratio = choice(exps)
                 row.append(
-                    f"{i+1}) {a}×{b}={' '*ANSWER_WIDTH}"
+                    f"{i+1}) {a*ratio}×{b}={' '*ANSWER_WIDTH}"
                     if randint(0, 3)
-                    else f"{i+1}) {a*b}:{a}={' '*ANSWER_WIDTH}"
+                    else f"{i+1}) {a*b*ratio}:{a}={' '*ANSWER_WIDTH}"
                 )
             writer.writerow(row)
 
